3210852e4cdf4a16386d34e83b3de9c16d23e2bb
Mahbub Alam <mahasayz@gmail.com>
2013-10-22 17:55:54 +0600

    SitePageBundle created. Base layout also done.

---
+++ src/Site/PageBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Site\PageBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SitePageBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
